6cb530f9f6a7b95fe73780f807c66005393c35f6
TallaBambo <tallabambo041@gmail.com>
2022-08-07 20:33:33 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
-  return (
-    <div className="App">
-      Hi
-    </div>
-  );
+  return <Routes >
+
+    <Route path='/' exact element={<p>This is the home page</p>} ></Route>
+    <Route path='/stared' exact element={<p>This is the starred page</p>} ></Route>
+    <Route path="*" element={<p>This is 404 page</p>}></Route>
+
+  </Routes>
+
 }
 
 export default App;
