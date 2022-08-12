a2b919e85556273b5dd1b8c78d6a72897fb82c53
TallaBambo <tallabambo041@gmail.com>
2022-08-12 12:47:04 +0100

    Created two new custom hooks ULQ and US

--- src/pages/Stared.js
+++ src/pages/Stared.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import ShowGrid from "../components/show/ShowGrid";
+import { apiGet } from "../misc/config";
+import { useShows } from "../misc/custom-hooks";
+
 const Starred = () => {
+
+    const [starred] = useShows()
+    const [shows, setShows] = useState(null)
+    const [isLoading, setIsloading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        if (starred && starred.length > 0) {
+            const promises = starred.map(showId => apiGet(`/show/${showId}`))
+
+            Promise.all(promises)
+                .then(apiData => apiData.map(show => ({ show })))
+                .then(results => {
+                    setShows(results)
+                    setIsloading(false)
+                }).catch(err => {
+                    setError(err.message)
+                    setIsloading(false)
+                })
+
+        } else {
+
+            setIsloading(false)
+
+        }
+    }, [starred])
     return (
         <MainPageLayout>
-            This is Starred page
+            {isLoading && <div>Shows are still loading</div>}
+            {error && <div>Error occured: {error}</div>}
+            {!isLoading && !shows && <div>No shows were added</div>}
+            {!isLoading && !error && shows && <ShowGrid data={shows} />}
         </MainPageLayout>
     )
 }
